9ca073df83287acfb853a910171d0fa575b922ac
Michał <ul0253566@edu.uni.lodz.pl>
2023-12-26 10:51:37 +0100

    bootstrap configuration with navigation

--- app/models/meme.rb
+++ app/models/meme.rb
@@ -1,6 +1,5 @@
 class Meme < ApplicationRecord
-    validates :title, presence: true
-    validates :image_url, presence: true
+    validates :title, presence: { message: "must be provided" }, length: { minimum: 5, maximum: 100, message: "should be between 5 and 100 characters" }
+    validates :image_url, presence: { message: "must be provided" }, format: { with: /\.(png|jpg|jpeg)\z/i, message: "must be a valid image format" }
     has_one_attached :image_url
-
 end
